df4f59aed827e12d16ad2e2f4da49baa88d8b5cd
Coda <Errorcodebin_2@hotmail.com>
2021-04-28 20:26:09 +0200

    Fixed configuration annotations not existing during runtime

--- src/main/java/nl/dacodadragon/slinkydog/configuration/annotations/SettingInfo.java
+++ src/main/java/nl/dacodadragon/slinkydog/configuration/annotations/SettingInfo.java
@@ -1,8 +1,11 @@
 package nl.dacodadragon.slinkydog.configuration.annotations;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD})
 public @interface SettingInfo {
     String nameInGame();
